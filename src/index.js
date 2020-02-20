9a4973697d81431419f3bf7a402c8295c5387dce
mvkozak <maryiakazak@gmail.com>
2020-02-20 16:22:19 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {return Math.min(...array);}
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return  0;
+  } else {return Math.max(...array);}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {return array.reduce((accumulator, currentValue) => (accumulator + currentValue),0)/array.length;}
+ 
 }
